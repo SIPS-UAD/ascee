05cdd72ed148bf07fc9d1421b84e52118e43e2d3
Yoga Rusydi Arifin <yogarifn@gmail.com>
2025-06-25 22:26:44 +0700

    mengganti logo

--- resources/js/components/atoms/nav-logo.tsx
+++ resources/js/components/atoms/nav-logo.tsx
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/react';
 const NavLogo = () => {
     return (
         <Link href="/" className="flex items-center justify-center">
-            <img src="/logo-ascee.svg" alt="ASCEE Logo" className="h-6" />
+            <img src="/logoascee.png" alt="ASCEE Logo" className="h-6" />
         </Link>
     );
 };
