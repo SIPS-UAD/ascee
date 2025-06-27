951942769e97d6c0385fc31fac635f1c2f3bb614
Yoga Rusydi Arifin <yogarifn@gmail.com>
2025-06-27 21:17:45 +0700

    fix galat pada logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,11 @@
-import AppLogoIcon from './app-logo-icon';
+import appLogo from '../../../public/logoascee.png'
+
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex items-center w-20 h-auto rounded-full ">
-                <img src="logoascee.png" alt="ASCEE Logo"/>
+                <img src={appLogo} alt="ASCEE Logo"/>
             </div>
         </>
     );
